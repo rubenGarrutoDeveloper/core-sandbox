203d9c6c110dafa57f123500e2b0df0549adf036
“ruben.garruto <r.garruto@reply.it>
2025-12-03 11:21:12 +0100

    initial setup

---
+++ src/main/java/com/spring_compendium/core_sandbox/CoreSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.spring_compendium.core_sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSandboxApplication.class, args);
+	}
+
+}
